553d29f0bdf9d143a6e4a4dade1002bd95046f99
thoang <thaong@gmail.com>
2025-03-29 19:38:47 +0700

    AD

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+from dotenv import load_dotenv
 import os
+
+# Load biến môi trường từ .env
+load_dotenv()
+
+# Đọc DATABASE_URL từ môi trường
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 print(f"Database URL: {DATABASE_URL}")  # Kiểm tra giá trị
