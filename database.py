e6becab6592c116949c146f4b9ba71eabe29df81
thoang <thaong@gmail.com>
2025-03-29 19:33:23 +0700

    ad

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+asyncmy://thoang:km123456@localhost:3310/fastapi"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 
