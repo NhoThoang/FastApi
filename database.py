a9ce3ac116f7f24385d17139d741c0f32f61a323
thoang <thaong@gmail.com>
2025-03-29 19:40:26 +0700

    Ad

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "mysql+asyncmy://thoang:km123456@mysql_container:3310/fastapi"
 
 
 
